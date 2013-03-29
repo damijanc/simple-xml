e43061268406016b99b9b6bd3ad395bd8303b6e5
Damijan Cavard <cavard@delo.si>
2013-03-29 14:18:22 +0100

    added more simple example

--- XmlDOM.test.php
+++ XmlDOM.test.php
@@ -2,6 +2,7 @@
 
 require_once 'XmlDOM.class.php';
 
+//complex example
 $arr = array(
                   '@attributes' => array(
                   'type' => 'fiction'
@@ -51,4 +52,21 @@ $dom->generateXML($arr,'books');
 $xml = $dom->saveXML();
 
 echo $xml;
+
+//and more simple example
+$arr = array(     'publishTime' => '18.01.2013 11:09:39',
+                      'date' => '18.01.2013',
+                      'title' => '18.01.2013',
+                      'publish' => 'False',
+                      'description' => 'delo',
+      );
+
+$dom = new XmlDOM('1.0', 'utf-8');
+$dom->formatOutput = true;
+$dom->generateXML($arr, 'issue');
+
+$xml = $dom->saveXML();
+
+echo $xml;
+
 ?>
